c0c3cd50a08e3fe7c3f99c2d90919efd91a85eb9
eezhar <eezhar@gmail.com>
2022-12-19 20:25:03 +0800

    player character animation created

--- mariozombie.js
+++ mariozombie.js
@@ -40,4 +40,53 @@ window.addEventListener("load", function () {
     }
   }
   const input = new InputHandler();
+
+  class Player {
+    // gameWidth and gameHeight to ensure the player character remains within the canvas boundaries
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      // sprite sheet size
+      this.width = 200; // sprite width
+      this.height = 200; // sprite height
+      this.x = 0;
+      this.y = 0;
+      this.image = document.getElementById("playerImage");
+      this.frameX = 0; // the frame within the sprite sheet
+      this.frameY = 0;
+      this.speed = 0; // positive will move the sprite right, negative left. this value is changed in the update() method
+    }
+    // specify which context we want to draw on. in case future changes involve multiple canvas contexts
+    draw(context) {
+      context.fillStyle = "white"; // background set to white for visibility
+      // position x and y of sprite in the context. Width and height of the sprite
+      context.fillRect(this.x, this.y, this.width, this.height);
+      // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight). image, dx, dy are compulsory parameters. the rest are optional
+      context.drawImage(
+        this.image,
+        this.frameX * this.width, // change the value of frameX and frameY to change to different sprite within the sprite sheet to animate movement
+        this.frameY * this.height, // this.width and this.height remains fixed since the sprite size is the same
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    update(input) {
+    //   this.x += this.speed;
+    }
+  }
+
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update();
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
